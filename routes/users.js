a2041b66de41143c95185657a1146b9ed32f4148
bhaskarAntonyCoder <codercss3958@gmail.com>
2023-10-13 12:30:28 +0530

    build included

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 // routes/user.js
 const express = require('express');
 const router = express.Router();
-const User = require('../models/users');
+const User = require('../models/users'); // Assuming you have a User model
 
 // Route to register a new user
 router.post('/register', async (req, res) => {
@@ -24,31 +24,35 @@ router.get('/list', async (req, res) => {
   }
 });
 
-//get user 
-app.get('/get-user', (req, res) => {
+// Get user by coupon code
+router.get('/get-user', async (req, res) => {
   const couponCode = req.query.couponCode;
-  const user = userData.find((u) => u.couponCode === couponCode);
-
-  if (!user) {
-    res.status(404).json({ message: 'User not found' });
-  } else {
-    res.json(user);
+  try {
+    const user = await User.findOne({ coupon: couponCode });
+    if (!user) {
+      res.status(404).json({ message: 'User not found' });
+    } else {
+      res.json(user);
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'Error finding the user check your internet connection' });
   }
 });
 
+// Delete user by ID
 router.delete('/delete/:userId', async (req, res) => {
-    try {
-      const influencerId = req.params.influencerId;
-      const deletedInfluencer = await Influencer.findByIdAndDelete(influencerId);
-  
-      if (!deletedInfluencer) {
-        return res.status(404).json({ error: 'user not found' });
-      }
-  
-      res.json({ message: 'user deleted successfully', deletedInfluencer });
-    } catch (error) {
-      res.status(500).json({ error: 'Could not delete influencer' });
+  try {
+    const userId = req.params.userId;
+    const deletedUser = await User.findByIdAndDelete(userId);
+
+    if (!deletedUser) {
+      return res.status(404).json({ error: 'User not found' });
     }
-  });
+
+    res.json({ message: 'User deleted successfully', deletedUser });
+  } catch (error) {
+    res.status(500).json({ error: 'Could not delete user' });
+  }
+});
 
 module.exports = router;
